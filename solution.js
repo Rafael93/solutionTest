72bf3215222ad26f77285ee0ad0de097f83bc92d
Rafael Gamarra <rafaelgamas@gmail.com>
2024-03-05 11:51:09 -0300

    change the iteration

--- solution.js
+++ solution.js
@@ -1,26 +1,26 @@
 const totalFocos = [
-  [100, 150, 141, 170], //561
+  [200, 150, 141, 170], //561
   [150, 180, 160, 120], //610
   [100, 101, 130, 150], //491
   [160, 102, 100, 200], //632
 ];
 
 const encontrarMenores = () => {
-  let menor = Infinity;
   let indicesMenor = [];
   for (let i = 0; i < totalFocos.length; i++) {
-    for (let j = 0; j < totalFocos[i].length; j++) {
-      if (totalFocos[i][j] < menor) {
-        menor = totalFocos[i][j];
-        indicesMenor.push([i, j]);
-      } else if (totalFocos[i][j] === menor) {
-        indicesMenor.push([i, j]); // Agregar los índices de los valores iguales al menor
-      }
-    }
+    totalFocos[i].sort();
   }
-  console.log(
-    `El menor es ${menor} y sus índices son ${JSON.stringify(indicesMenor)}`
-  );
+
+  console.log(totalFocos);
 };
 
 encontrarMenores();
+
+/* for (let j = 0; j < totalFocos[i].length; j++) {
+	if (totalFocos[i][j] < menor) {
+		menor = totalFocos[i][j];
+		indicesMenor.push([i, j]);
+	} else if (totalFocos[i][j] === menor) {
+		indicesMenor.push([i, j]); // Agregar los índices de los valores iguales al menor
+	}
+} */
