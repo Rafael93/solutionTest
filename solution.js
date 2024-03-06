b970b61f69e13871990b19037aa908fd96098fd4
Rafael Gamarra <rafaelgamas@gmail.com>
2024-03-06 07:11:50 -0300

    reemplazarTubos

--- solution.js
+++ solution.js
@@ -5,22 +5,69 @@ const totalFocos = [
   [160, 102, 100, 200], //632
 ];
 
+let menor = Infinity;
+let segundoMenor = Infinity;
+let cantidadFocosReemplazados = 0;
+let horasPorAño = 2700;
 const encontrarMenores = () => {
-  let indicesMenor = [];
   for (let i = 0; i < totalFocos.length; i++) {
     totalFocos[i].sort();
+    for (let j = 0; j < totalFocos[i].length - 2; j++) {
+      if (totalFocos[i][j] < menor && totalFocos[i][j + 1] < segundoMenor) {
+        menor = totalFocos[i][j];
+        segundoMenor = totalFocos[i][j + 1];
+        console.log(
+          `El menor es: ${menor} y el segundo menor es: ${segundoMenor} y estan en la unidad ${
+            i + 1
+          }`
+        );
+      }
+    }
   }
+};
 
+const consumirHoras = (horaMasBaja, segundaHoraMasBaja) => {
+  for (let i = 0; i < totalFocos.length; i++) {
+    for (let j = 0; j < totalFocos[i].length; j++) {
+      if (totalFocos[i][j] - horaMasBaja >= 0 && horaMasBaja !== 0) {
+        totalFocos[i][j] = totalFocos[i][j] - horaMasBaja;
+      } else if (
+        totalFocos[i][j] - segundaHoraMasBaja >= 0 &&
+        segundaHoraMasBaja !== 0
+      ) {
+        totalFocos[i][j] = totalFocos[i][j] - segundaHoraMasBaja;
+      } else {
+        return;
+      }
+    }
+  }
   console.log(totalFocos);
 };
 
-encontrarMenores();
+const rand = () => {
+  return Math.floor(Math.random() * 101) + 100;
+};
 
-/* for (let j = 0; j < totalFocos[i].length; j++) {
-	if (totalFocos[i][j] < menor) {
-		menor = totalFocos[i][j];
-		indicesMenor.push([i, j]);
-	} else if (totalFocos[i][j] === menor) {
-		indicesMenor.push([i, j]); // Agregar los índices de los valores iguales al menor
-	}
-} */
+const reemplazarTubos = () => {
+  let contador = 0;
+  for (let i = 0; i < totalFocos.length; i++) {
+    totalFocos[i].sort();
+    //evaluar fila
+    for (let j = 0; j < totalFocos[i].length; j++) {
+      if (totalFocos[i][j] === 0) {
+        contador++;
+      }
+      if (contador === 2) {
+        //detener el recorrido
+        j = totalFocos[i].length;
+        totalFocos[i] = totalFocos[i].map(() => rand());
+        cantidadFocosReemplazados += 4;
+      }
+    }
+    //termina la fila
+    contador = 0;
+  }
+};
+
+encontrarMenores();
+consumirHoras(menor, segundoMenor);
